73d242a7332435260f6c6ed681df6a9ac55e03e9
Jayesh Hire <jayeshhire96@gmail.com>
2023-08-16 21:58:42 +0530

    added comments for understanding of code for beginners

--- timer-cont.c
+++ timer-cont.c
@@ -31,12 +31,21 @@ void main(void){
 }
 
 void delay(){
-	
+	// using the timer 0 and mode 1
 	TMOD  =0x01;
+	
+	//setting the time value for about 50 msec
 	TH0 = 0x4B;
 	TL0 = 0xFD;
+	
+	//timer start
 	TR0 = 1;
-	if (TF0 == 1)
+	
+	//check for timer overflow 
+	if (TF0 == 1){
+	//stop timer
 		TR0 = 0 ;
-	TF0 = 0;
+		//reset overflow flag
+		TF0 = 0;
+	}
 }
